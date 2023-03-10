a951793e09460d20dd42d59cf525668508d7b61d
Dmitriy <plotnikovdi95@gmail.com>
2023-03-10 17:54:06 +0300

    fix

--- src/api/const.js
+++ src/api/const.js
@@ -3,6 +3,5 @@ export const URL_AUTH = 'https://www.reddit.com/api/v1/authorize?';
 export const CLIENT_ID = 'r1PjkmpcZqBP9Efamg_0kQ';
 export const REPSONSE_TYPE = 'token';
 export const RANDOM_STRING = 'random_string';
-// export const REDIRECT_URI = 'http://localhost:3000/auth';
 export const REDIRECT_URI = 'https://blogget-weld.vercel.app/auth';
 export const SCOPE = 'identity submit read';
